97855dff207dc22a8187f7db510dc43e13dbb146
bigyandulal2 <rdravid569@gmail.com>
2025-10-31 11:16:17 +0545

    updating

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/portfolio">
+      <BrowserRouter basename="/">
         <AnimatedRoutes />
       </BrowserRouter>
     </TooltipProvider>
