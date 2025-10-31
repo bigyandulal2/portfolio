e679b1305a90851b41bb9a69542b1dcdb753ff29
bigyandulal2 <rdravid569@gmail.com>
2025-10-31 11:25:31 +0545

    adding here routes

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <AnimatedRoutes />
       </BrowserRouter>
     </TooltipProvider>
