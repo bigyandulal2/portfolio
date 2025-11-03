da21b8cb244df26712c4c7a9aa702e839a186661
bigyandulal2 <rdravid569@gmail.com>
2025-11-03 12:26:38 +0545

    adding fixes

--- src/components/ui/copyright.tsx
+++ src/components/ui/copyright.tsx
@@ -1,6 +1,6 @@
 export default function Copyright() {
     return (
-      <div className='absolute top-[30%] left-0 md:left-5  z-[999] text-pink-500 text-center [writing-mode:vertical-rl] rotate-180'>
+      <div className='absolute top-[30%] left-0  md:left-5  z-[999] text-pink-500 text-center [writing-mode:vertical-rl] rotate-180'>
         &copy; All Rights  Reserved Bigyan Dulal {new Date().getFullYear()}
       </div>
     )
